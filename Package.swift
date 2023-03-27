828db2457f434c4b4c1a43982d2ff629d3958831
Ignacio Olave <olave.ia@icloud.com>
2023-03-26 21:21:41 -0300

    learning

--- Package.swift
+++ Package.swift
@@ -8,6 +8,9 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        // .package(url: "https://github.com/mtynior/ColorizeSwift.git", from: "1.2.0"),
+        // .package(url: "https://github.com/Swiftline/Swiftline.git", from: "0.5.0"),
+        // .package(url: "https://github.com/jatoben/CommandLine", from: "2.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- Sources/icloud-bridge/icloud_bridge.swift
+++ Sources/icloud-bridge/icloud_bridge.swift
@@ -3,6 +3,12 @@ public struct icloud_bridge {
     public private(set) var text = "Hello, World!"
 
     public static func main() {
-        print(icloud_bridge().text)
+        
+        print(CommandLine.arguments)
+        print(CommandLine.argc)
     }
+}
+
+private func cli_handler (args: Array<String>) -> String {
+    return "";
 }

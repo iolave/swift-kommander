79312f853dc082bc62d6602f0f1a01585c312537
Ignacio Olave <olave.ia@icloud.com>
2023-04-17 20:10:15 -0400

    - created a new command file - migrated CommandOption - created a CLICommandBase class to allow simple commands

---
+++ Sources/CLIKommander/command.swift
@@ -0,0 +1,37 @@
+import Foundation;
+
+public class CLICommandBase {
+    let name: String;
+    var action: CLIAction? = nil;
+    var options: [CLIOption]? = nil;
+
+    init(name: String, action: @escaping CommandAction) {
+        self.name = name;
+        self.action = action;
+    }
+
+    init(name:String, action: @escaping CommandAction, options: [CLIOption]) {
+        self.name = name;
+        self.action = action;
+        self.options = options;
+    }
+}
+
+public struct CLIOption {
+    let name: String;
+    let requiresValue: Bool;
+    let required: Bool;
+    
+    init(name: String, requiresValue: Bool, required: Bool) {
+        if (!name.hasPrefix("--")) {
+            print("CLIOptions 'name' property is required to have a '--' prefix");
+            exit(1);
+        }
+
+        self.name = name;
+        self.requiresValue = requiresValue;
+        self.required = required;
+    }
+}
+
+public typealias CLIAction = () -> Void;

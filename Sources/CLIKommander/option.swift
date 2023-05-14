1c68f26b19f543e25fdaec1bee0d4684485bd275
Ignacio Olave <olave.ia@icloud.com>
2023-05-13 22:55:44 -0400

    moves CLIOption related code to it's own file

---
+++ Sources/CLIKommander/option.swift
@@ -0,0 +1,64 @@
+public struct CLIOption {
+    let name: String;
+    let shorthand: String?;
+    let requiresValue: Bool;
+    let required: Bool;
+    
+    init(name: String, requiresValue: Bool, required: Bool) throws {
+        if (!name.hasPrefix("--")) {
+            print("CLIOptions 'name' property is required to have a '--' prefix");
+            throw CLIOptionError.invalidNamePrefix;
+        }
+
+        self.name = name;
+        self.requiresValue = requiresValue;
+        self.required = required;
+        self.shorthand = nil;
+    }
+
+    init(name: String, shorthand: String ,requiresValue: Bool, required: Bool) throws {
+        if (!name.hasPrefix("--")) {
+            print("CLIOptions 'name' property is required to have a '--' prefix");
+            throw CLIOptionError.invalidNamePrefix;
+        }
+
+        if(!shorthand.hasPrefix("-")) {
+            print("CLIOptions 'shorthand' property is required to have a '-' prefix");
+            throw CLIOptionError.invalidShorthandPrefix;
+        }
+
+        self.name = name;
+        self.requiresValue = requiresValue;
+        self.required = required;
+        self.shorthand = shorthand;
+    }
+}
+
+enum CLIOptionError: Error {
+    case invalidNamePrefix
+    case invalidShorthandPrefix
+}
+
+/**
+ * Check if a CLIOptions array have a duplicated
+ * `name` and/or `shorthand` property.
+ * - Parameters:
+ * - Returns:
+ * `true` when a duplicate is found and `false` otherwise.
+ */
+public func optionDuplicates(options: [CLIOption]) -> Bool {
+    var mutableOptions: [CLIOption] = options;
+
+    while(mutableOptions.count > 0) {
+        let opt: CLIOption? = mutableOptions.popLast();
+
+        if (opt == nil) { return true }
+
+        for optToCheck: CLIOption in mutableOptions {
+            if (optToCheck.name == opt!.name) { return true }
+            if (optToCheck.shorthand == opt!.shorthand) { return true }
+        }
+    }
+
+    return false;
+}
---
+++ Tests/CLIKommanderTests/optionTests.swift
@@ -0,0 +1,65 @@
+import XCTest
+@testable import CLIKommander
+
+final class CLIOptionTests: XCTestCase {
+    func test_init_error_throw_invalidNamePrefix() {
+        let name: String = "test-opt";
+
+        do {
+            _ = try CLIOption(name: name, requiresValue: true, required: true);
+        } catch CLIOptionError.invalidNamePrefix {
+            return;
+        } catch {
+            XCTFail("Wrong error thrown");
+            return;
+        }
+            
+        XCTFail("No error thown");
+    }
+
+    func test_init_error_throw_invalidShorthandPrefix() {
+        let name: String = "--test-opt";
+        let shorthand: String = "test";
+
+        do {
+            _ = try CLIOption(name: name, shorthand: shorthand, requiresValue: true, required: true);
+        } catch CLIOptionError.invalidShorthandPrefix {
+            return;
+        } catch {
+            XCTFail("Wrong error thrown");
+            return;
+        }
+            
+        XCTFail("No error thown");
+    }
+
+    func test_init_success() {
+        let name: String = "--test-opt";
+        let shorthand: String = "-t";
+
+        XCTAssertNoThrow(try CLIOption(name: name, shorthand: shorthand, requiresValue: true, required: true))
+    }
+}
+
+final class optionDuplicatesTests: XCTestCase {
+    func test_duplicates_true() throws {
+        let name: String = "--test-opt";
+
+        let opts: [CLIOption] = [
+            try CLIOption(name: name, requiresValue: true, required: true),
+            try CLIOption(name: name, requiresValue: true, required: true),
+        ];
+
+        XCTAssert(optionDuplicates(options: opts) == true);
+    }
+
+    func test_duplicates_false() throws {
+        let name: String = "--test-opt";
+
+        let opts: [CLIOption] = [
+            try CLIOption(name: name, requiresValue: true, required: true),
+        ];
+
+        XCTAssert(optionDuplicates(options: opts) == false);
+    }
+}

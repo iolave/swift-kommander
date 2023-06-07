97d10d944eae4a84d6d84e04037b05a303b2c7aa
Ignacio Olave <olave.ia@icloud.com>
2023-06-07 12:14:13 -0400

    adds docs to CLIKommander init method

--- Sources/CLICommand/kommander.swift
+++ Sources/CLICommand/kommander.swift
@@ -1,4 +1,8 @@
 public final class CLIKommander: CLICommand {
+	/** Creates a CLIKommander instance with an
+		app name as `default`. If the app name 
+		requires to be modified, please refer to
+		the `setAppName` method */
 	public init () {
 		super.init("default")
 	}
